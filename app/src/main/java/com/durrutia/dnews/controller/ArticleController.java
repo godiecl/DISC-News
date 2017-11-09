f6333727b0b351e04e9ea3defe537c02b0643eef
Diego Urrutia Astorga <durrutia@ucn.cl>
2017-11-09 20:34:13 -0300

    Added: Controller and this tests.

---
+++ app/src/main/java/com/durrutia/dnews/controller/ArticleController.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2017 by Diego Urrutia-Astorga http://durrutia.com
+ *
+ * This work is licensed under the Creative Commons Attribution 4.0 International License.
+ * To view a copy of this license, visit http://creativecommons.org/licenses/by/4.0/.
+ */
+
+package com.durrutia.dnews.controller;
+
+import com.durrutia.dnews.model.Article;
+import com.durrutia.dnews.model.NewsApi;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+import java.util.List;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+/**
+ * Clase Principal que contiene los metodos de acceso a las noticias.
+ */
+public final class ArticleController {
+
+    /**
+     * Des-Serializador GSON
+     */
+    private static final Gson gson = new GsonBuilder()
+            .serializeNulls()
+            .setPrettyPrinting() // TODO: Eliminar en modo produccion
+            .create();
+
+    /**
+     * URL desde donde se obtendran los {@link Article}.
+     */
+    private static final String url = "https://newsapi.org/v1/articles?source=techcrunch&sortBy=latest&apiKey=638da3177b274f35a68cc0c539e4d294";
+
+    /**
+     * Cliente OkHttp
+     */
+    private final OkHttpClient okHttpClient = new OkHttpClient();
+
+    /**
+     * Obtencion de {@link Article}s desde Internet.
+     *
+     * @return the {@link List} of {@link Article}.
+     */
+    public List<Article> getArticles() throws IOException {
+
+        // Peticion
+        final Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        // Respuesta
+        final Response response = okHttpClient.newCall(request).execute();
+        final String json = response.body().string();
+
+        final NewsApi newsApi = gson.fromJson(json, NewsApi.class);
+
+        return newsApi.getArticles();
+
+    }
+
+
+}
---
+++ app/src/test/java/com/durrutia/dnews/TestNewsController.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2017 by Diego Urrutia-Astorga http://durrutia.com
+ *
+ * This work is licensed under the Creative Commons Attribution 4.0 International License.
+ * To view a copy of this license, visit http://creativecommons.org/licenses/by/4.0/.
+ */
+
+package com.durrutia.dnews;
+
+import com.durrutia.dnews.controller.ArticleController;
+import com.durrutia.dnews.model.Article;
+
+import org.apache.commons.lang3.time.StopWatch;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author Diego Urrutia Astorga.
+ */
+@Slf4j
+public final class TestNewsController {
+
+    /**
+     * @throws IOException
+     */
+    @Test
+    public void testConnection() throws IOException {
+
+        final StopWatch stopWatch = StopWatch.createStarted();
+        log.debug("Starting test connection ..");
+
+        final ArticleController nc = new ArticleController();
+        final List<Article> articles = nc.getArticles();
+        Assertions.assertThat(articles).isNotNull();
+        Assertions.assertThat(articles).hasSize(10);
+
+        for (final Article article : articles) {
+            log.debug("Article: {}", article);
+        }
+
+        log.info("Test in: {}", stopWatch);
+
+    }
+
+}
