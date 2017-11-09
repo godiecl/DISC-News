a40369f788b2ecfc24ec6b1eb6c2764ad780ee4d
Diego Urrutia Astorga <durrutia@ucn.cl>
2017-11-09 20:38:56 -0300

    Added: Singleton and his declaration.

---
+++ app/src/main/java/com/durrutia/dnews/App.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2017 by Diego Urrutia-Astorga http://durrutia.com
+ * This work is licensed under the Creative Commons Attribution 4.0 International License.
+ * To view a copy of this license, visit http://creativecommons.org/licenses/by/4.0/
+ */
+package com.durrutia.dnews;
+
+import android.app.Application;
+
+/**
+ * @author Diego Urrutia Astorga on 2017-11-09.
+ */
+public final class App extends Application {
+
+    /**
+     *
+     */
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
