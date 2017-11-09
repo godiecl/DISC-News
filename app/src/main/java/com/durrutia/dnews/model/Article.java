1d1bc2a48905e75c0ad222bc15da46587387f633
Diego Urrutia Astorga <durrutia@ucn.cl>
2017-11-09 19:59:45 -0300

    Added: Clases obtenidas via json to pojo.

---
+++ app/src/main/java/com/durrutia/dnews/model/Article.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2017 by Diego Urrutia-Astorga http://durrutia.com
+ *
+ * This work is licensed under the Creative Commons Attribution 4.0 International License.
+ * To view a copy of this license, visit http://creativecommons.org/licenses/by/4.0/.
+ */
+
+package com.durrutia.dnews.model;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import lombok.Builder;
+import lombok.Getter;
+
+/**
+ * Clase generada via http://www.jsonschema2pojo.org/
+ */
+@Builder
+public final class Article {
+
+    @Getter
+    private String author;
+
+    @Getter
+    private String title;
+
+    @Getter
+    private String description;
+
+    @Getter
+    private String url;
+
+    @Getter
+    private String urlToImage;
+
+    @Getter
+    private String publishedAt;
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+    }
+
+}
---
+++ app/src/main/java/com/durrutia/dnews/model/NewsApi.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2017 by Diego Urrutia-Astorga http://durrutia.com This work is licensed under the Creative Commons Attribution 4.0 International License. To view a copy of this license, visit http://creativecommons.org/licenses/by/4.0/
+ */
+
+package com.durrutia.dnews.model;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.Builder;
+import lombok.Getter;
+
+/**
+ * Clase generada via http://www.jsonschema2pojo.org/
+ */
+@Builder
+public final class NewsApi {
+
+    @Getter
+    private String status;
+
+    @Getter
+    private String source;
+
+    @Getter
+    private String sortBy;
+
+    @Getter
+    private List<Article> articles = new ArrayList<Article>();
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+    }
+
+}
